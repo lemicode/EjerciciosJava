1d4309476bb93157029325dcbf9e76bc0c0847c8
Marco Vanegas <example@example.com>
2023-12-04 17:11:15 -0500

    first commit

---
+++ Ejercicios/Operadores/src/operadores/Operadores.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadores;
+
+/**
+ *
+ * @author marcovanegas
+ */
+public class Operadores {
+    
+    /**
+     * Aritméticos
+     * Relacionales
+     * Condicionales
+     */
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int num1, num2, resultado;
+    
+        num1 = 4;
+        num2 = 2;
+        
+        resultado = num1 + num2;
+        
+        System.out.println("El resultado de la suma es: " + resultado);
+        
+    }
+    
+}
