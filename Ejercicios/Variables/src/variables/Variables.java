62abae852b4b58b2772b98b377718dbe7ec67d64
Marco Vanegas <example@example.com>
2023-12-04 17:14:56 -0500

    Ajustes

---
+++ Ejercicios/Variables/src/variables/Variables.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package variables;
+
+/**
+ *
+ * @author marcovanegas
+ */
+public class Variables {
+    
+    /**
+     * Int
+     * Double
+     * Boolean
+     * Char
+     * String
+     * Long
+     */
+    
+    int numero = 5;
+    String nombre = "Ibra"; // Comillas dobles
+    char letra = 'L'; // Comillas simples
+    double temperatura = 38.5;
+    boolean siONo = false;
+    long largo = 1234456789;
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
